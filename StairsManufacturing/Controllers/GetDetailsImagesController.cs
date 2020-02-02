3e64c6925fd98eeb60307596a7c838dc050d798f
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-02-03 02:27:11 +0300

    Начал работу с передачей на бэк ID категории, которую выбрал пользователь, и переход на страницу конкретных изображений с деталями

---
+++ StairsManufacturing/Controllers/GetDetailsImagesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StairsManufacturing.Models;
+using StairsManufacturing.Models.Index;
+
+namespace StairsManufacturing.Controllers {
+    public class GetDetailsImagesController : Controller {
+        public IActionResult GetDetails(IndexModel model) {
+            return View();
+        }
+    }
+}
